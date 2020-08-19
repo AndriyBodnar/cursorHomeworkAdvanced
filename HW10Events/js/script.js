76af26e8b2ddb6f450a754d72395825632108780
Andriy <powerlviv@ukr.net>
2020-08-19 16:41:22 +0300

    test hw10

--- HW10Events/js/script.js
+++ HW10Events/js/script.js
@@ -1,77 +1,88 @@
-window.addEventListener("keydown", function (el) {
+function playSound(el) {
   const audio = document.querySelector(`audio[class = "key${el.keyCode}"]`);
   const key = document.querySelector(`div[id = "key${el.keyCode}"]`);
   if (!audio) return;
   audio.play();
   audio.currentTime = 0;
   key.classList.add("play");
-});
-
-const keys = document.querySelectorAll(".keyWord");
-keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+}
 
 function removeTransition(el) {
   if (el.propertyName !== "transform") return;
   this.classList.remove("play");
 }
 
-document.getElementById("key65").addEventListener("click", function () {
-  console.log(document.getElementById("key65"));
-  const audio = document.querySelector(`audio[class = key65]`);
-  const key = document.querySelector(`div[id = key65]`);
-  audio.play();
-  key.classList.add("play");
-});
+const keys = document.querySelectorAll(".keyWord");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 
-document.getElementById("key83").addEventListener("click", function () {
-  console.log(document.getElementById("key83"));
-  const audio = document.querySelector(`audio[class = key83]`);
-  const key = document.querySelector(`div[id = key83]`);
+window.addEventListener("keyup", playSound);
+
+// click
+
+const btn = document.getElementById("key72");
+
+function playSoundClick(numberKey){
+    console.log(document.getElementById(`${numberKey}`));
+  const audio = document.querySelector(`audio[class = ${numberKey}]`);
+  const key = document.querySelector(`div[id = ${numberKey}]`);
   audio.play();
   audio.currentTime = 0;
   key.classList.add("play");
-});
+}
 
-document
-  .getElementById("key68")
-  .addEventListener("click", function keyClick(key68) {
-    console.log(document.getElementById("key68"));
-    const audio = document.querySelector(`audio[class = key68]`);
-    const key = document.querySelector(`div[id = key68]`);
-    audio.play();
-    audio.currentTime = 0;
-    key.classList.add("play");
-  });
+const seventyTwo = playSoundClick(`key72`);
 
-document
-  .getElementById("key70")
-  .addEventListener("click", function keyClick(key68) {
-    console.log(document.getElementById("key70"));
-    const audio = document.querySelector(`audio[class = key70]`);
-    const key = document.querySelector(`div[id = key70]`);
-    audio.play();
-    audio.currentTime = 0;
-    key.classList.add("play");
-  });
+btn.addEventListener("click", seventyTwo);
 
-document
-  .getElementById("key71")
-  .addEventListener("click", function keyClick(key68) {
-    console.log(document.getElementById("key71"));
-    const audio = document.querySelector(`audio[class = key71]`);
-    const key = document.querySelector(`div[id = key71]`);
-    audio.play();
-    audio.currentTime = 0;
-    key.classList.add("play");
-  });
 
-document
-  .getElementById("key72")
-  .addEventListener("click", function keyClick(key68) {
-    console.log(document.getElementById("key72"));
-    const audio = document.querySelector(`audio[class = key72]`);
-    const key = document.querySelector(`div[id = key72]`);
-    audio.play();
-    audio.currentTime = 0;
-    key.classList.add("play");
-  });
+
+// let arr = document.querySelectorAll(".key");
+// let div_array = Array.prototype.slice.call(arr);
+
+// console.log(arr);
+// document.getElementById("key65").addEventListener("click", playMusic(0));
+// document.getElementById("key83").addEventListener("click", playMusic(1));
+// document.getElementById("key68").addEventListener("click", playMusic(2));
+// document.getElementById("key70").addEventListener("click", playMusic(3));
+// document.getElementById("key71").addEventListener("click", playMusic(4));
+
+// function playMusic(el) {
+//   const firstMusic = arr[el];
+//   firstMusic.play();
+//   firstMusic.currentTime = 0;
+//   // document.getElementById(`${b}`).classList.add("play");
+// }
+
+// debugger
+
+// const playSoundClick = (numberKey) => {
+//   console.log(document.getElementById(`${numberKey}`));
+//   const audio = document.querySelector(`audio[class = ${numberKey}]`);
+//   const key = document.querySelector(`div[id = ${numberKey}]`);
+//   audio.play();
+//   audio.currentTime = 0;
+//   key.classList.add("play");
+// }
+const btn = document.getElementById("key72");
+
+function playSoundClick(numberKey){
+    console.log(document.getElementById(`${numberKey}`));
+  const audio = document.querySelector(`audio[class = ${numberKey}]`);
+  const key = document.querySelector(`div[id = ${numberKey}]`);
+  audio.play();
+  audio.currentTime = 0;
+  key.classList.add("play");
+}
+
+const seventyTwo = playSoundClick(`key72`);
+
+btn.addEventListener("click", seventyTwo);
+
+
+
+// console.log(document.getElementById(`key72`));
+// const audio = document.querySelector(`audio[class = key72]`);
+// const key = document.querySelector(`div[id = key72]`);
+// audio.play();
+// audio.currentTime = 0;
+// key.classList.add("play");
